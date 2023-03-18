338debf99f2cd9a5dfbcc9361a842d84fd9a833d
CavinKavi <104263022+CavinKavi@users.noreply.github.com>
2023-03-18 19:11:38 +0800

    slight styling

---
+++ SC2006/js/carbookingpage.js
@@ -0,0 +1,88 @@
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-firestore.js";
+import { getStorage, ref } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-storage.js";
+import { collection, addDoc, getDocs, Timestamp  } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD4zU0-igHFj_TDWXuTZ9BMwbtr8Z_kHkM",
+    authDomain: "fir-c9c47.firebaseapp.com",
+    databaseURL: "https://fir-c9c47.firebaseio.com",
+    projectId: "fir-c9c47",
+    storageBucket: "fir-c9c47.appspot.com",
+    messagingSenderId: "732611892783",
+    appId: "1:732611892783:web:518baf14f8076858af18da",
+    storageBucket: "gs://fir-c9c47.appspot.com"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore
+const db = getFirestore(app);
+
+// Initialize Cloud Storage
+const storage = getStorage(app);
+const storageRef = ref(storage);
+
+
+
+//buttons
+var bookBtn = document.querySelector("#bookBtn");
+
+//book button event listener
+bookBtn.addEventListener('click', BookCar);
+
+
+//functions           
+async function BookCar()
+{
+    //get date and time selected by user
+    var StartTrip = document.getElementById("StartTrip").value;
+    var EndTrip = document.getElementById("EndTrip").value;
+
+    //add into db, under "Bookings" Collection => need to change to users collection
+    try {
+        const docRef = await addDoc(collection(db, "Bookings"), {
+                    
+            Start: StartTrip,
+            End: EndTrip
+        });
+        console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+            console.error("Error adding document: ", e);
+    }
+            
+}
+
+    
+
+
+
+
+/*
+            //writing into db
+            try {
+                const docRef = await addDoc(collection(db, "TestCars"), {
+                    first: "Alan",
+                    last: "Mathison",
+                    born: 1912
+            });
+            console.log("Document written with ID: ", docRef.id);
+            } catch (e) {
+                console.error("Error adding document: ", e);
+            }
+            */
+
+            //reading from db
+            /*
+            const querySnapshot = await getDocs(collection(db, "TestCars"));
+            querySnapshot.forEach((doc) => {
+                console.log(`${doc.id} => ${doc.data()}`);
+            });
+*/
