d0b2fa6aed5f92704f264db9dc7d9cdccb683d9b
wm <wmjipyok@gmail.com>
2023-03-31 23:39:28 +0800

    added star rating to car list

--- SC2006/js/car-list.js
+++ SC2006/js/car-list.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = firebase.firestore();
-
+const starsTotal = 5;
 
 $(function () {
     $("#nav-content").load("nav.html");
@@ -36,7 +36,7 @@ window.addEventListener('load', function () {
 function getCarList() {
     db.collection("Cars").where("Carpark", "array-contains", parseFloat(lat)).orderBy("CreateDateTime", "desc").get().then(snapshot => {
         if (snapshot.docs.length > 0) {
-            snapshot.docs.forEach(doc => {
+            snapshot.docs.forEach((doc, index) => {
                 if (doc.exists && doc.data().Status == 0) {
                     let data = doc.data();
                     // console.log("snapshot:" + doc.data().CreateDateTime);
@@ -45,47 +45,52 @@ function getCarList() {
                         if (docRef.exists) {
                             userName = docRef.data().FirstName;
                         }
+
+                        const starPercentage = (data.Rating / starsTotal * 100);
+                        const starPercentageRounded = `${Math.round(starPercentage / 10) * 10}%`;
+
                         document.getElementById("carListContainer").innerHTML += `
-                    <div class="col-xl-3 col-md-6 mb-4">
-                        <div class="card border-bottom-primary shadow h-100 py-2">
-                            <a href="car-detail.html?carId=${doc.id}" style="text-decoration: none">
-                                <div class="card-body">
-                                    <div class="col-auto" style="margin-bottom: 10px; margin-left: auto;margin-right: auto;">
-                                            <img src="${data.Images[0]} class="col-auto"
-                                                style="width:100%;">
-                                    </div>
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="h4 mb-0 font-weight-bold text-gray-800">
-                                                $${data.Price}/day
-                                            </div>
-                                            <div class="h5 mb-0 font-weight text-gray-800">
-                                                ${data.Brand} ${data.Model}
-                                            </div>
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-bottom-primary shadow h-100 py-2">
+                                <a href="car-detail.html?carId=${doc.id}" style="text-decoration: none">
+                                    <div class="card-body">
+                                        <div class="col-auto" style="margin-bottom: 10px; margin-left: auto;margin-right: auto;">
+                                                <img src="${data.Images[0]} class="col-auto"
+                                                    style="width:100%;">
                                         </div>
-
-                                        <div class="col-auto">
-                                            <div class="h4 mb-0 font-weight text-gray-800">
-                                                ${data.Seats} seater
+                                        <div class="row no-gutters align-items-center">
+                                            <div class="col mr-2">
+                                                <div class="h4 mb-0 font-weight-bold text-gray-800">
+                                                    $${data.Price}/day
+                                                </div>
+                                                <div class="h5 mb-0 font-weight text-gray-800">
+                                                    ${data.Brand} ${data.Model}
+                                                </div>
                                             </div>
 
-                                            <i class="fa fa-star fa-xs"></i>
-                                            <i class="fa fa-star fa-xs"></i>
-                                            <i class="fa fa-star fa-xs"></i>
-                                            <i class="fa fa-star fa-xs"></i>
-                                            <i class="fa fa-star fa-xs"></i>
+                                            <div class="col-auto">
+                                                <div class="h4 mb-0 font-weight text-gray-800">
+                                                    ${data.Seats} seater
+                                                </div>
+
+                                                <div class="stars-outer">
+                                                    <div class="stars-inner" style="width: ${starPercentageRounded}"></div>
+                                                </div>
+                                                <span class="number-rating" style="color: grey">${data.Rating}</span>
+                                            </div>
+                                        </div>
+                                        <div class="h7 mb-0 font-weight text-gray-800" style="margin-top:5px;">
+                                            <img class="owner-profile rounded-circle" style="width:20px; vertical-align: text-top;"
+                                                    src="img/profile.png">
+                                            <span id="CarOwner">${userName}</span>
                                         </div>
                                     </div>
-                                    <div class="h7 mb-0 font-weight text-gray-800" style="margin-top:5px;">
-                                        <img class="owner-profile rounded-circle" style="width:20px; vertical-align: text-top;"
-                                                src="img/profile.png">
-                                        <span id="CarOwner">${userName}</span>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>`
+                                </a>
+                            </div>
+                        </div>`
                     });
+
+
                 }
             })
             document.getElementById("loading").setAttribute('hidden', true);
